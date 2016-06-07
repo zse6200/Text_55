ed7296df27e6e440e0a886c5dd5a0ca100cf991f
lanou3g <510623775@qq.com>
2016-06-07 22:40:50 +0800

    000

---
+++ Text55/Text55/New2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  New2ViewController.h
+//  Text55
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 张明杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface New2ViewController : UIViewController
+
+@end
---
+++ Text55/Text55/New2ViewController.m
@@ -0,0 +1,37 @@
+//
+//  New2ViewController.m
+//  Text55
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 张明杰. All rights reserved.
+//
+
+#import "New2ViewController.h"
+
+@interface New2ViewController ()
+
+@end
+
+@implementation New2ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
